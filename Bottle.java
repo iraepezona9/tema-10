ae3526696f52f54cb873dc7300286b3e165f036b
iraepezona9 <126812367+iraepezona9@users.noreply.github.com>
2023-04-11 04:12:41 -0700

    Add files via upload

---
+++ Bottle.java
@@ -0,0 +1,14 @@
+public class Bottle {
+    private boolean hasMoreLiquid;
+    private int avalaibleLiquid;
+
+    public boolean hasMoreLiquid()
+    {
+        return hasMoreLiquid;
+    }
+    
+    public int getAvailableLiquid()
+    {
+        return availableLiquid;
+    }
+
